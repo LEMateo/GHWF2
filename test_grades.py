29e29ce271e684ca1b441a11502418f7cf1c05ac
Daniel Stocker <stdrs02@moravian.edu>
2019-02-01 13:48:46 -0500

    Added parameter for dorpping grades and modified tests to support it.

--- test_grades.py
+++ test_grades.py
@@ -4,14 +4,14 @@ from grades import compute_hw_average
 
 def test_zero_grades():
     grades = []
-    assert compute_hw_average(grades) == 0
+    assert compute_hw_average(grades, 0) == 0
 
 
 def test_single_grade():
     grades = [42]
-    assert compute_hw_average(grades) == 42
+    assert compute_hw_average(grades, 0) == 42
 
 
 def test_two_grades():
     grades = [42, 42]
-    assert compute_hw_average(grades) == 42
+    assert compute_hw_average(grades, 0) == 42
